2e07f6d0290cf973f8b140f20d42321a117aba68
Nurdin Imanaliev <nurdin.imanaliev@iaau.edu.kg>
2020-04-03 11:15:50 +0600

    initial commit

---
+++ src/main/java/kg/nurdin/finalbackend/FinalBackEndApplication.java
@@ -0,0 +1,13 @@
+package kg.nurdin.finalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalBackEndApplication.class, args);
+    }
+
+}
